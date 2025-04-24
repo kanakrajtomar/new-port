78ce0e60f126ac0f2261b94cf8bb1b50d19a3057
Kanak Raj Tomar <kanakrajtomar6@gmail.com>
2025-04-25 03:27:00 +0530

    Initial commit of developer portfolio code

---
+++ app/layout.js
@@ -0,0 +1,66 @@
+// import { GoogleTagManager } from "@next/third-parties/google";
+// import { Inter } from "next/font/google";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import Footer from "./components/footer";
+// import ScrollToTop from "./components/helper/scroll-to-top";
+// import Navbar from "./components/navbar";
+// import "./css/card.scss";
+// import "./css/globals.scss";
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata = {
+//   title: "Portfolio of Abu Said - Software Developer",
+//   description:
+//     "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>
+//         <ToastContainer />
+//         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+//           <Navbar />
+//           {children}
+//           <ScrollToTop />
+//         </main>
+//         <Footer />
+//       </body>
+//       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+//     </html>
+//   );
+// }
+import { GoogleTagManager } from "@next/third-parties/google";
+import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/footer";
+import ScrollToTop from "./components/helper/scroll-to-top";
+import Navbar from "./components/navbar";
+import "./css/card.scss";
+import "./css/globals.scss";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Portfolio of Kanak Raj Tomar - Software Developer",
+  description:
+    "Welcome to the portfolio of Kanak Raj Tomar. I am a software developer passionate about problem-solving and creating innovative solutions. With experience in full-stack development and cloud technologies, I aim to make impactful contributions to tech projects. I enjoy working with JavaScript and love exploring new tech stacks.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ToastContainer />
+        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+          <Navbar />
+          {children}
+          <ScrollToTop />
+        </main>
+        <Footer />
+      </body>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+    </html>
+  );
+}
---
+++ utils/data/contactsData.js
@@ -0,0 +1,10 @@
+export const contactsData = {
+    email: 'kanakrajtomar6@gmail.com',
+    phone: '+91 7248667895',
+    address: 'Punjab, India',
+    github: 'https://github.com/kanakrajtomar',
+    linkedIn: 'https://www.linkedin.com/in/kanak-raj-tomar',
+    twitter: 'https://twitter.com/kanakrajtomar',
+    stackOverflow: 'https://stackoverflow.com/users/kanakrajtomar',
+    devUsername: "kanakrajtomar"
+}
